b2182f31c51ce236ce9c1499eb52aed5908e663f
Angel Ilivanov <angel.ilivanov2006@gmail.com>
2026-08-11 18:16:13 +0200

    fix: listener errors no longer ignored

--- internal/authserver/listener.go
+++ internal/authserver/listener.go
@@ -2,6 +2,7 @@ package authserver
 
 import (
 	"fmt"
+	"log"
 	"net"
 
 	"github.com/angel-ilivanov/jaraxxus-wow/internal/authserver/protocol"
@@ -10,7 +11,7 @@ import (
 func (s *Server) Start(port string) {
 	listener, err := net.Listen("tcp", port)
 	if err != nil {
-		fmt.Printf("Error establishing listener %s", err)
+		log.Fatalf("error establishing listener %v", err)
 	}
 	defer listener.Close()
 	fmt.Println("Listening for TCP Connections")
@@ -18,6 +19,7 @@ func (s *Server) Start(port string) {
 		conn, err := listener.Accept()
 		if err != nil {
 			fmt.Printf("Error accepting connection: %s", err)
+			continue
 		}
 		go s.handleConnection(conn)
 	}
