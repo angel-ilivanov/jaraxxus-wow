f18116df79c0f74dd38f8d1bb755ac62ff41ac83
Angel Ilivanov <angel.ilivanov2006@gmail.com>
2026-08-11 15:27:32 +0200

    fix: resolved crash upon ending connection

--- internal/authserver/listener.go
+++ internal/authserver/listener.go
@@ -6,8 +6,6 @@ import (
 	"io"
 	"log"
 	"net"
-
-	"github.com/angel-ilivanov/jaraxxus-wow/internal/authserver/protocol"
 )
 
 func (s *Server) Start(port string) {
@@ -33,6 +31,9 @@ func (s *Server) handleConnection(conn net.Conn) {
 	for {
 		header := make([]byte, 4)
 		_, err := io.ReadFull(conn, header) // Keeps reading until array is full
+		if err == io.EOF {
+			return
+		}
 		if err != nil {
 			log.Fatalf("error reading header %v", err)
 			return
@@ -41,6 +42,9 @@ func (s *Server) handleConnection(conn net.Conn) {
 		size := binary.LittleEndian.Uint16(header[2:4]) // length without header
 		body := make([]byte, size)
 		_, err = io.ReadFull(conn, body)
+		if err == io.EOF {
+			return
+		}
 		if err != nil {
 			log.Fatalf("error reading body %v", err)
 			return
