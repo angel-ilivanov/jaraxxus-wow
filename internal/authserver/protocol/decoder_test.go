4fc073e1a14b27f63afbbd35583c967a2875c451
Angel Ilivanov <angel.ilivanov2006@gmail.com>
2026-08-12 20:14:32 +0200

    test: wrote unit tests for decoder.go

---
+++ internal/authserver/protocol/decoder_test.go
@@ -0,0 +1,46 @@
+package protocol
+
+import (
+	"bytes"
+	"testing"
+)
+
+var logonChallengeClientPacket = []byte{
+	0x00,       // opcode
+	0x08,       // protocol version 8
+	0x26, 0x00, // size
+	'W', 'o', 'W', 0,
+	3, 3, 5, // version
+	0x34, 0x30, // build 12340 little-endian
+	'6', '8', 'x', 0,
+	'n', 'i', 'W', 0,
+	'S', 'U', 'n', 'e',
+	0, 0, 0, 0, // timezone offset
+	127, 0, 0, 1, // ip
+	8,                                      // account name length
+	'J', 'A', 'R', 'A', 'X', 'X', 'U', 'S', // account name
+}
+
+func TestReadClientMessageReturnsCorrectMessageType(t *testing.T) {
+	result, _ := ReadClientMessage(bytes.NewReader(logonChallengeClientPacket))
+	_, ok := result.(LogonChallengeClientRequest)
+	if !ok {
+		t.Fatalf("Expected a LogonChallengeClientRequest, but got %T", result)
+	}
+}
+
+func TestDecodeUsername(t *testing.T) {
+	result, _ := ReadClientMessage(bytes.NewReader(logonChallengeClientPacket))
+	name := result.(LogonChallengeClientRequest).AccountName
+	if name != "JARAXXUS" {
+		t.Fatalf("Expected JARAXXUS but got %s", name)
+	}
+}
+
+func TestDecodeIp(t *testing.T) {
+	result, _ := ReadClientMessage(bytes.NewReader(logonChallengeClientPacket))
+	ip := result.(LogonChallengeClientRequest).Ip
+	if ip != 2130706433 {
+		t.Fatalf("Expected Ip 2130706433 but got %d", ip)
+	}
+}
