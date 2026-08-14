c4a1940f5e8a5ac6bec61c5f8037aa1ec9b09ef0
Angel Ilivanov <angel.ilivanov2006@gmail.com>
2026-08-14 14:50:55 +0200

    refactor: renamed functions and introduced saltSize constant

--- internal/authserver/srp6/srp6.go
+++ internal/authserver/srp6/srp6.go
@@ -8,6 +8,7 @@ import (
 )
 
 const generator = 7
+const saltSize = 32
 
 var largeSafePrimeLittleEndian = []byte{
 	0xb7, 0x9b, 0x3e, 0x2a, 0x87, 0x82, 0x3c, 0xab,
@@ -15,38 +16,38 @@ var largeSafePrimeLittleEndian = []byte{
 	0x53, 0x50, 0x06, 0x29, 0x8b, 0x5b, 0xad, 0xbd,
 	0x5b, 0x53, 0xe1, 0x89, 0x5e, 0x64, 0x4b, 0x89,
 }
-var n = bytesToInt(largeSafePrimeLittleEndian)
-var g = big.NewInt(generator)
+var largeSafePrime = bytesToBigInt(largeSafePrimeLittleEndian)
+var generatorBigInt = big.NewInt(generator)
 
 func generateSalt() []byte {
-	salt := make([]byte, 32)
-	rand.Read(salt) // never returns an error
+	salt := make([]byte, saltSize)
+	_, _ = rand.Read(salt) // never returns an error
 	return salt
 }
 
 // returns little endian password verifier
 func calculatePasswordVerifier(username string, password string, salt []byte) []byte {
-	x := bytesToInt(calculateX(username, password, salt))
-	return intToBytes(32, big.NewInt(0).Exp(g, x, n))
+	x := bytesToBigInt(calculateX(username, password, salt))
+	return bigIntToBytes(saltSize, big.NewInt(0).Exp(generatorBigInt, x, largeSafePrime))
 }
 
-// salt and X are in little endian
+// x = SHA1( s | SHA1( U | : | p )),
 func calculateX(username string, password string, salt []byte) []byte {
-	interim := sha1.Sum([]byte(strings.ToUpper(username) + ":" + strings.ToUpper(password)))
-	concatenated := append(salt[:], interim[:]...)
-	hash := sha1.Sum(concatenated)
-	return hash[:]
+	credsHash := sha1.Sum([]byte(strings.ToUpper(username) + ":" + strings.ToUpper(password)))
+	concatenated := append(salt[:], credsHash[:]...)
+	combinedHash := sha1.Sum(concatenated)
+	return combinedHash[:]
 }
 
 // Adapted from Kangaroux/go-wow-srp6, endian.go:
 // https://github.com/Kangaroux/go-wow-srp6/blob/7a61e15fd8d75f4ebe8ac91e07eef140219ef8ff/endian.go
-// bytesToInt returns a little endian big integer from a big endian byte array.
-func bytesToInt(data []byte) *big.Int {
+// bytesToBigInt returns a little endian big integer from a big endian byte array.
+func bytesToBigInt(data []byte) *big.Int {
 	return big.NewInt(0).SetBytes(reverse(data))
 }
 
-// intToBytes returns a big endian byte array from a little endian big integer.
-func intToBytes(padding int, bi *big.Int) []byte {
+// bigIntToBytes returns a big endian byte array from a little endian big integer.
+func bigIntToBytes(padding int, bi *big.Int) []byte {
 	return reverse(pad(padding, bi.Bytes()))
 }
 
