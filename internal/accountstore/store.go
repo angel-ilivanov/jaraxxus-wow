b6ae1eab8c9e87df73bcf2b4c96ff21a1830e134
Angel Ilivanov <angel.ilivanov2006@gmail.com>
2026-08-14 16:35:30 +0200

    fix: insert stores uppercased usernames

--- internal/accountstore/store.go
+++ internal/accountstore/store.go
@@ -56,7 +56,7 @@ func (s *Store) Insert(
 		 ON CONFLICT ON CONSTRAINT account_username_unique DO NOTHING
 		 RETURNING id`,
 		pgx.StrictNamedArgs{
-			"username": username,
+			"username": strings.ToUpper(username),
 			"salt":     credentials.Salt,
 			"verifier": credentials.Verifier,
 		},
