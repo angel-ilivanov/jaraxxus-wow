ff109a49debffefadb9dd64cc646fbf422398762
Angel Ilivanov <54551188+angel-ilivanov@users.noreply.github.com>
2026-08-16 17:12:25 +0200

    Implement client proof packet parsing (#6)
    * feat: decode logon proof packet
    
    * test: implement unit tests for decoding the client proof packet

---
+++ internal/authserver/protocol/logon_proof_client.go
@@ -0,0 +1,68 @@
+package protocol
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"io"
+)
+
+type logonProofWire struct {
+	Opcode          uint8
+	ClientPublicKey [32]uint8
+	ClientProof     [20]uint8
+	CrcHash         [20]uint8
+	NumKeys         uint8
+	SecurityFlags   uint8
+}
+
+type LogonProofRequest struct {
+	clientPublicKey []byte
+	clientProof     []byte
+}
+
+func (l LogonProofRequest) isRequest() {}
+
+func DecodeLogonProofRequest(reader io.Reader) (LogonProofRequest, error) {
+	packetBytes, err := readLogonProofPacket(reader)
+	if err != nil {
+		return LogonProofRequest{}, fmt.Errorf("error decoding logon proof request: %w", err)
+	}
+	wire, err := decodeLogonProofWire(packetBytes)
+	if err != nil {
+		return LogonProofRequest{}, fmt.Errorf("error decoding logon proof request: %w", err)
+	}
+	return newLogonProofRequest(wire), nil
+}
+
+func readLogonProofPacket(reader io.Reader) ([]byte, error) {
+	packet := make([]byte, 74)
+	_, err := io.ReadFull(reader, packet)
+	if err != nil {
+		return nil, fmt.Errorf("error reading logon proof packet: %w", err)
+	}
+	packet = append([]byte{byte(CmdAuthLogonProof)}, packet...) //prepend opcode
+	fmt.Println("Client Proof Packet:", packet)
+	fmt.Println("size:", len(packet))
+	return packet, nil
+}
+
+func decodeLogonProofWire(packetBytes []byte) (logonProofWire, error) {
+	var parsed logonProofWire
+	reader := bytes.NewReader(packetBytes)
+	err := binary.Read(reader, binary.LittleEndian, &parsed)
+	if err != nil {
+		return logonProofWire{}, fmt.Errorf("error decoding packet bytes into wire object: %w", err)
+	}
+	fmt.Println("client public key:", parsed.ClientPublicKey)
+	fmt.Println("client proof:", parsed.ClientProof)
+	fmt.Println("2fa enabled:", parsed.SecurityFlags)
+	return parsed, nil
+}
+
+func newLogonProofRequest(wire logonProofWire) LogonProofRequest {
+	return LogonProofRequest{
+		clientPublicKey: wire.ClientPublicKey[:],
+		clientProof:     wire.ClientProof[:],
+	}
+}
