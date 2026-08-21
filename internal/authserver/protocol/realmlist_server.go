554f5e4d5294e1b2480ac973eb739c41f6fa6570
Angel Ilivanov <fruzont@gmail.com>
2026-08-21 19:19:12 +0300

    refactor(realmlist encoding): make realmName constant string, formatting

--- internal/authserver/protocol/realmlist_server.go
+++ internal/authserver/protocol/realmlist_server.go
@@ -5,13 +5,15 @@ import (
 	"encoding/binary"
 )
 
-var headerPadding = make([]byte, 4)
-var realmName = []byte("Jaraxxus\x00")
-var population = []byte{0x00, 0x00, 0x00, 0x00} //MEDIUM
-var footerPadding = make([]byte, 2)
-var numberOfRealms = []byte{0x01, 0x00} // one
+var (
+	headerPadding  = make([]byte, 4)
+	population     = []byte{0x00, 0x00, 0x00, 0x00} //MEDIUM
+	footerPadding  = make([]byte, 2)
+	numberOfRealms = []byte{0x01, 0x00} // one (little endian)
+)
 
 const (
+	realmName             = "Jaraxxus\x00"
 	flagRecommendedRealm  = 0x20
 	categoryEuropeEnglish = 0x01
 	realmId               = 0x01
@@ -27,8 +29,13 @@ type RealmListResponse struct {
 func (r RealmListResponse) isResponse() {}
 
 func EncodeRealmListResponse(response RealmListResponse) []byte {
-	addressPort := append([]byte(response.WorldServerAddress), 0x00) // Append \0x00 byte
-	var size = uint16(18 + len(realmName) + len(addressPort))        // packet size without opcode and size
+	addressPort := append([]byte(
+		response.WorldServerAddress),
+		0x00) // Append \0x00 byte
+	var size = uint16(
+		18 +
+			len(realmName) +
+			len(addressPort)) // packet size without opcode and size
 	packetSizeLittleEndian := make([]byte, 2)
 	binary.LittleEndian.PutUint16(packetSizeLittleEndian, size)
 
@@ -40,7 +47,7 @@ func EncodeRealmListResponse(response RealmListResponse) []byte {
 	buf.WriteByte(realmTypePvp)
 	buf.WriteByte(realmLockDisabled)
 	buf.WriteByte(flagRecommendedRealm)
-	buf.Write(realmName)
+	buf.WriteString(realmName)
 	buf.Write(addressPort)
 	buf.Write(population)
 	buf.WriteByte(response.NumChars)
