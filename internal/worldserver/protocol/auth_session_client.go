74548e2426b913a11e010fe2f9631eec76053c1d
Angel Ilivanov <fruzont@gmail.com>
2026-08-26 12:32:46 +0300

    feat(CMSG_AUTH_SESSION): read username from packet

--- internal/worldserver/protocol/auth_session_client.go
+++ internal/worldserver/protocol/auth_session_client.go
@@ -1,12 +1,16 @@
 package protocol
 
 import (
+	"bufio"
+	"bytes"
+	"encoding/binary"
 	"fmt"
 	"io"
 )
 
-type authSessionWire struct {
-	build uint32
+type authSessionWire1 struct {
+	Build         uint32
+	LoginServerId uint32
 }
 
 type authSessionRequest struct {
@@ -23,6 +27,7 @@ func DecodeAuthSession(size uint16, reader io.Reader) (ClientMessage, error) {
 		return nil, err
 	}
 	fmt.Println(packet)
+	decodeAuthSessionWire(packet)
 	return nil, nil
 }
 
@@ -32,6 +37,28 @@ func readAuthSessionPacket(size uint16, reader io.Reader) ([]byte, error) {
 	if err != nil {
 		return nil, fmt.Errorf("error reading CMSG_AUTH_SESSION packet bytes: %w", err)
 	}
-	fmt.Println(body)
 	return body, nil
+}
+
+func decodeAuthSessionWire(packetBytes []byte) {
+	var wire1 authSessionWire1
+	reader := bytes.NewReader(packetBytes)
+	err := binary.Read(reader, binary.LittleEndian, &wire1)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	_, err = readUsername(reader)
+	if err != nil {
+		return
+	}
+}
+
+func readUsername(reader io.Reader) (string, error) {
+	br := bufio.NewReader(reader)
+	username, err := br.ReadBytes(0x00)
+	if err != nil {
+		return "", fmt.Errorf("error reading username: %w", err)
+	}
+	return string(username), nil
 }
