1370dfbb2387ef035cf9689b2844bd8e85cb1c0c
Angel Ilivanov <fruzont@gmail.com>
2026-08-26 18:50:48 +0300

    feat(accountStore): implement fetchSessionKey function

--- internal/accountstore/store.go
+++ internal/accountstore/store.go
@@ -124,3 +124,22 @@ func (s *Store) SetSessionKey(
 
 	return nil
 }
+
+func (s *Store) FetchSessionKey(ctx context.Context, username string) ([]byte, error) {
+	commandTag, err := s.db.Exec(
+		ctx,
+		`SELECT session_key FROM public.account WHERE username = @username`,
+		pgx.StrictNamedArgs{
+			"username": username,
+		},
+	)
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			return nil, fmt.Errorf("find session key for account: %w", ErrNotFound)
+		}
+
+		return nil, fmt.Errorf("find session key for account: %w", err)
+	}
+
+	return []byte(commandTag.String()), nil
+}
