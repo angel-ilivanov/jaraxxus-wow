2cf4bdc243cc3dc691338d3caffb0d4ab3288593
Angel Ilivanov <fruzont@gmail.com>
2026-09-03 18:57:33 +0300

    fix(charEnum): encode character enum

--- internal/worldserver/char_enum_handler.go
+++ internal/worldserver/char_enum_handler.go
@@ -2,11 +2,16 @@ package worldserver
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/angel-ilivanov/jaraxxus-wow/internal/worldserver/protocol"
 )
 
 func (handler RequestHandler) handleCharEnum(ctx context.Context, session *session, request protocol.CharEnumRequest) (protocol.CharEnumResponse, error) {
 	// TODO: Fetch account characters
-	return protocol.CharEnumResponse{AmountOfCharacters: 0}, nil
+	characters, err := handler.characterStore.ListByAccountId(ctx, session.AccountId)
+	if err != nil {
+		return protocol.CharEnumResponse{}, fmt.Errorf("error retrieving characters: %w", err)
+	}
+	return protocol.CharEnumResponse{Characters: characters}, nil
 }
